edd301c24f6d6e93d46c889d518fc3a17c8b138c
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-09-19 17:35:38 +0200

    [NEW] SEOS-483: Adding python test for the pre-provisioned keystore.

---
+++ test_preprovisioned_key_store.py
@@ -0,0 +1,43 @@
+import pytest
+
+import sys
+sys.path.append('../common')
+
+import logs
+import os
+import re
+import time
+
+test_imagepath = '/build-zynq7000-Debug-PRE_PROVISIONED_KEYSTORE/images/capdl-loader-image-arm-zynq7000'
+timeout = 10
+
+@pytest.mark.parametrize("img, expected_output_array, timeout", [
+    (test_imagepath,
+    [
+        'Preprovisioning keystore demo succeeded'
+    ],
+    timeout),
+])
+
+def test_key_store_provisioning(boot_with_proxy, img, expected_output_array, timeout):
+    mqtt_proxy_memory_file='nvm_06'
+    pre_provisioned_keystore_image='preProvisionedKeyStoreImg'
+
+    #remove the existing mqtt_proxy_demo memory file
+    if os.path.isfile(mqtt_proxy_memory_file):
+        os.remove(mqtt_proxy_memory_file)
+
+    #place the prepared keystore image in its place
+    if os.path.isfile(pre_provisioned_keystore_image):
+        os.rename(pre_provisioned_keystore_image, mqtt_proxy_memory_file)
+    else:
+        print("ERROR: Could not find the pre-preovisioned keystore image!")
+        assert os.path.isfile(pre_provisioned_keystore_image)
+
+    f_out = boot_with_proxy(image_subpath=img)[1]
+
+    for index in range(0, len(expected_output_array)):
+        success = expected_output_array[index]
+        (text, match) = logs.get_match_in_line(f_out, re.compile(success), timeout)
+        print(text)
+        assert match == success
