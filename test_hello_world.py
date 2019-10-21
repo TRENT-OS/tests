2235f15700f2e24afed1309efa595c3a5654a2fb
Axel Heider <axel.heider@hensoldt-cyber.com>
2019-10-21 16:41:50 +0200

    [DEV] enable hallo world test again

--- test_hello_world.py
+++ test_hello_world.py
@@ -8,12 +8,18 @@ import os
 import re
 import time
 
+image = "build-zynq7000-Debug-TEST_HELLO_WORLD/images/capdl-loader-image-arm-zynq7000"
+timeout = 15
 
-#def test_hello_world(boot):
-#    test_run = boot("build-zynq7000-Debug-HELLO_WORLD/images/capdl-loader-image-arm-zynq7000")
-#    f_out = test_run[1]
-#
-#    success_msg = "hello world!"
-#    timeout = 60
-#    (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
-#    assert match == success_msg
+
+# ToDo: actually, this test case should run first and if it fails, then the
+#       whole test should be aborted.
+def test_hello_world(boot):
+    test_run = boot(image)
+    f_out = test_run[1]
+
+    success_msg = "hello world!"
+    timeout = 60
+    (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
+    assert match == success_msg
+
