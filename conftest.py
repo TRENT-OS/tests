de41adcb9d6b60880799464aa15c2fa73a944336
Axel Heider <axel.heider@hensoldt-cyber.com>
2019-10-22 05:11:14 +0200

    [DEV] create unified helper function start_qemu_and_proxy()

--- conftest.py
+++ conftest.py
@@ -36,6 +36,78 @@ def terminate_process_by_pid_file(
 
 
 #-------------------------------------------------------------------------------
+def start_qemu_and_proxy(
+    image_path,
+    out_file,
+    in_file,
+    hout_file,
+    herr_file,
+    qemu_pid_file_name,
+    proxy_path = None,
+    pout_file = None,
+    proxy_pid_file_name = None
+):
+    print('guest output file is ' + out_file)
+    print('host input file is ' + in_file)
+    print('host output file is ' + hout_file)
+    print('host err file is ' + herr_file)
+    if pout_file is not None:
+        print('proxy out file is ' + pout_file)
+
+    if not os.path.isfile(qemu_pid_file_name):
+        print("launching qemu on " + image_path)
+        start_process_and_create_pid_file(
+            "qemu-system-arm" +
+                " -machine xilinx-zynq-a9" +
+                " -nographic" +
+                " -s" +
+                " -S" +
+                " -serial pty" +
+                " -serial file:" + out_file +
+                " -m size=1024M" +
+                " -kernel " + image_path +
+                " 2>" + herr_file +
+                " >" + hout_file +
+                " <" + in_file,
+            qemu_pid_file_name)
+        print("process detached")
+
+        time.sleep(1)
+
+    f_err   = logs.open_file_non_blocking(herr_file, "r")
+    f_in    = open(in_file, "w", buffering=1)
+    f_out   = None
+
+    if proxy_path is not None:
+        (text, match) = logs.get_match_in_line(f_err, re.compile('(\/dev\/pts\/\d)'), 10)
+        assert(match)
+
+        if not os.path.isfile(proxy_pid_file_name):
+            print("Start proxy...")
+            start_process_and_create_pid_file(
+                proxy_path + " " + match + " > " + pout_file,
+                proxy_pid_file_name)
+            print("Detached proxy app process")
+
+    cont = True
+    attempts = 0
+    while cont:
+        connected = False
+        attempts += 1
+        try:
+            f_in.write("c\n")
+            connected = True
+        except IOError as e:
+            time.sleep(1)
+        cont = not connected and attempts < 60
+
+    time.sleep(2)
+    f_out = logs.open_file_non_blocking(out_file, 'r', '\r')
+
+    return (f_in, f_out, f_err)
+
+
+#-------------------------------------------------------------------------------
 @pytest.fixture(scope="module")
 def boot(workspace_path):
     d = tempfile.mkdtemp()
@@ -50,57 +122,13 @@ def boot(workspace_path):
     os.mkfifo(in_file)
 
     def boot_image_path(image_subpath):
-
-        image_path = os.path.join(workspace_path, image_subpath)
-
-        print('guest output file is ' + out_file)
-        print('host input file is ' + in_file)
-        print('host output file is ' + hout_file)
-        print('host err file is ' + herr_file)
-
-        if not os.path.isfile(qemu_pid_file_name):
-            print("launching qemu on " + image_path)
-            start_process_and_create_pid_file(
-                "qemu-system-arm" \
-                    " -machine xilinx-zynq-a9"\
-                    " -nographic"\
-                    " -s"\
-                    " -S"\
-                    " -serial pty"\
-                    " -serial file:" + out_file + \
-                    " -m size=1024M"\
-                    " -kernel " + image_path + \
-                    " 2>" + herr_file + \
-                    " >" + hout_file + \
-                    " <" + in_file,
-                qemu_pid_file_name)
-            print("process detached")
-
-            time.sleep(1)
-
-        f_err   = logs.open_file_non_blocking(herr_file, "r")
-        f_in    = open(in_file, "w", buffering=1)
-        f_out   = None
-
-        (text, match) = logs.get_match_in_line(f_err, re.compile('(\/dev\/pts\/\d)'), 10)
-        assert(match)
-
-        cont = True
-        attempts = 0
-        while cont:
-            connected = False
-            attempts += 1
-            try:
-                f_in.write("c\n")
-                connected = True
-            except IOError as e:
-                time.sleep(1)
-            cont = not connected and attempts < 60
-
-        time.sleep(2)
-        f_out = logs.open_file_non_blocking(out_file, 'r', '\r')
-
-        return (f_in, f_out, f_err)
+        return start_qemu_and_proxy(
+                    os.path.join(workspace_path, image_subpath),
+                    out_file,
+                    in_file,
+                    hout_file,
+                    herr_file,
+                    qemu_pid_file_name)
 
     yield boot_image_path
 
@@ -126,65 +154,16 @@ def boot_with_proxy(workspace_path, proxy_path):
     os.mkfifo(in_file)
 
     def boot_image_path(image_subpath):
-        image_path = os.path.join(workspace_path, image_subpath)
-
-        print('guest output file is ' + out_file)
-        print('host input file is ' + in_file)
-        print('host output file is ' + hout_file)
-        print('host err file is ' + herr_file)
-        print('proxy out file is ' + pout_file)
-
-
-        if not os.path.isfile(qemu_pid_file_name):
-            print("launching qemu on " + image_path)
-            start_process_and_create_pid_file(
-                "qemu-system-arm" \
-                    " -machine xilinx-zynq-a9" \
-                    " -nographic" \
-                    " -s" \
-                    " -S" \
-                    " -serial pty" \
-                    " -serial file:" + out_file + \
-                    " -m size=1024M" \
-                    " -kernel " + image_path + \
-                    " 2>" + herr_file + \
-                    " >" + hout_file +
-                    " <" + in_file,
-                qemu_pid_file_name)
-            print("process detached")
-
-            time.sleep(1)
-
-        f_err   = logs.open_file_non_blocking(herr_file, "r")
-        f_in    = open(in_file, "w", buffering=1)
-        f_out   = None
-
-        (text, match) = logs.get_match_in_line(f_err, re.compile('(\/dev\/pts\/\d)'), 10)
-        assert(match)
-
-        if not os.path.isfile(proxy_pid_file_name):
-            print("Start proxy...")
-            start_process_and_create_pid_file(
-                proxy_path + " " + match + " > " + pout_file,
-                proxy_pid_file_name)
-            print("Detached proxy app process")
-
-        cont = True
-        attempts = 0
-        while cont:
-            connected = False
-            attempts += 1
-            try:
-                f_in.write("c\n")
-                connected = True
-            except IOError as e:
-                time.sleep(1)
-            cont = not connected and attempts < 60
-
-        time.sleep(2)
-        f_out = logs.open_file_non_blocking(out_file, 'r', '\r')
-
-        return (f_in, f_out, f_err)
+        return start_qemu_and_proxy(
+                    os.path.join(workspace_path, image_subpath),
+                    out_file,
+                    in_file,
+                    hout_file,
+                    herr_file,
+                    qemu_pid_file_name,
+                    proxy_path,
+                    pout_file,
+                    proxy_pid_file_name)
 
     yield boot_image_path
 
