321ffcbca8fcc6f0c30bbc28babcd3c3e8883c15
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2019-12-03 10:36:55 +0100

    [NEW] SEOS-458: Created PY file for TLS test

---
+++ test_tls_api.py
@@ -0,0 +1,27 @@
+import pytest
+
+import sys
+sys.path.append('../common')
+
+import logs
+import os
+import re
+import time
+import socket
+
+test_system = "test_tls_api"
+timeout = 180
+
+def test_tls_api_library(boot_with_proxy):
+    test_run = boot_with_proxy(test_system)
+    f_out = test_run[1]
+    success = 'testTls_lib: OK'
+    (text, match) = logs.get_match_in_line(f_out, re.compile(success), timeout)
+    assert match == success
+
+def test_tls_api_rpc(boot_with_proxy):
+    test_run = boot_with_proxy(test_system)
+    f_out = test_run[1]
+    success = 'testTls_rpc: OK'
+    (text, match) = logs.get_match_in_line(f_out, re.compile(success), timeout)
+    assert match == success
