3715f0ed650ae8ccfc8f69837b808a837260c911
Goran Badevic <goran.badevic@hensoldt-cyber.com>
2019-12-19 17:26:05 +0100

    [DEV] add test python script to run the configuration server demo

---
+++ test_demo_configuration_as_component.py
@@ -0,0 +1,15 @@
+import pytest
+import sys
+import os
+import re
+sys.path.append('../common')
+import logs
+
+def test_run_demo(boot_with_proxy):
+    test_run = boot_with_proxy("demo_configuration_as_component")
+    f_out = test_run[1]
+
+    success_msg = "finished successfully"
+    timeout = 15
+    (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
+    assert match == success_msg
