59c2f2dc6ffb71bfb64c2b48d4914177b3abcff4
Sebastian Eckl <sebastian.eckl@hensoldt-cyber.com>
2020-01-30 20:58:17 +0100

    [DEV] add first single test instances

--- test_partition_manager.py
+++ test_partition_manager.py
@@ -5,8 +5,33 @@ import re
 sys.path.append('../common')
 import logs
 
-def test_run_demo(boot_with_proxy):
+timeout = 15
 
+# def test_run_demo(boot_with_proxy):
+
+#     proxy_memory_file = "nvm_06"
+#     if os.path.isfile(proxy_memory_file):
+#         os.remove(proxy_memory_file)
+#     with  open(proxy_memory_file,"wb") as f:
+#         # create 36 MiB NVM file. The size comes from the partition sizes that
+#         # are defined in the define SEOS system config file. One day we should
+#         # extend things, so the SEOS system can request the proxy to create a
+#         # NVM of a given size and that is can quesry the currenty NVM size and
+#         # fail if this is too small. For now, we have to sync the value here
+#         # with the SEOSM system manually.
+#         f.seek( (36*1024*1024) -1 )
+#         f.write(b"\0")
+
+#     test_run = boot_with_proxy("test_partition_manager")
+#     f_out = test_run[1]
+
+#     success_msg = "# end #"
+#     timeout = 30
+#     (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
+#     assert match == success_msg
+
+
+def test_partition_manager_get_info_disk(boot_with_proxy):
     proxy_memory_file = "nvm_06"
     if os.path.isfile(proxy_memory_file):
         os.remove(proxy_memory_file)
@@ -23,7 +48,10 @@ def test_run_demo(boot_with_proxy):
     test_run = boot_with_proxy("demo_partition_manager")
     f_out = test_run[1]
 
-    success_msg = "# end #"
-    timeout = 30
-    (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
-    assert match == success_msg
+    result_list = [
+        'test_partition_manager_get_info_disk: OK',
+        'test_partition_manager_get_info_disk_wrong_return: OK',
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result    
