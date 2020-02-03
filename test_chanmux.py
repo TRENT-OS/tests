e83d3fc16d7865cecd3e44d9edbdb679dc7ad750
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-02-03 14:31:57 +0100

    [DEV] Provide implementation of test_chanmux_overflow() in test_chanmux.py

--- test_chanmux.py
+++ test_chanmux.py
@@ -17,7 +17,14 @@ def test_chanmux_overflow(boot_with_proxy):
     test_run = boot_with_proxy(test_system)
     f_out = test_run[1]
 
-    assert False, "NOT YET IMPLEMENTED"
+    expected_output_array = [
+        "ChanMuxTest_testOverflow: SUCCESS",
+        "ChanMuxTest_testOverflow: SUCCESS"
+    ]
+
+    for success_msg in expected_output_array:
+        (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
+        assert match == success_msg
 
 #@pytest.mark.skip(reason="backend not yet implemented")
 #-------------------------------------------------------------------------------
