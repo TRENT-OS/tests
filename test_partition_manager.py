c05b7366d4e9c7ab791c28c0cbffb8e54305ed43
Sebastian Eckl <sebastian.eckl@hensoldt-cyber.com>
2020-02-03 16:12:09 +0100

    [DEV] add test cases & short test description in Readme

--- test_partition_manager.py
+++ test_partition_manager.py
@@ -7,30 +7,6 @@ import logs
 
 timeout = 15
 
-# def test_run_demo(boot_with_proxy):
-
-#     proxy_memory_file = "nvm_06"
-#     if os.path.isfile(proxy_memory_file):
-#         os.remove(proxy_memory_file)
-#     with  open(proxy_memory_file,"wb") as f:
-#         # create 36 MiB NVM file. The size comes from the partition sizes that
-#         # are defined in the define SEOS system config file. One day we should
-#         # extend things, so the SEOS system can request the proxy to create a
-#         # NVM of a given size and that is can quesry the currenty NVM size and
-#         # fail if this is too small. For now, we have to sync the value here
-#         # with the SEOSM system manually.
-#         f.seek( (36*1024*1024) -1 )
-#         f.write(b"\0")
-
-#     test_run = boot_with_proxy("test_partition_manager")
-#     f_out = test_run[1]
-
-#     success_msg = "# end #"
-#     timeout = 30
-#     (text, match) = logs.get_match_in_line(f_out, re.compile(success_msg), timeout)
-#     assert match == success_msg
-
-
 def test_partition_manager_get_info_disk(boot_with_proxy):
     proxy_memory_file = "nvm_06"
     if os.path.isfile(proxy_memory_file):
@@ -49,9 +25,37 @@ def test_partition_manager_get_info_disk(boot_with_proxy):
     f_out = test_run[1]
 
     result_list = [
-        'test_partition_manager_get_info_disk: OK',
-        'test_partition_manager_get_info_disk_wrong_return: OK',
+        'TestPMInit_get_info_disk: OK'
     ]
     for result in result_list:
         (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
         assert match == result    
+
+#-------------------------------------------------------------------------------
+
+def test_partition_manager_get_info_disk_fake_disk(boot_with_proxy):
+    test_run = boot_with_proxy("test_partition_manager")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestPMInit_get_info_disk_fake_disk: OK'
+        
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+
+#-------------------------------------------------------------------------------
+
+def test_partition_manager_get_info_disk_fail(boot_with_proxy):
+    test_run = boot_with_proxy("test_partition_manager")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestPMInit_get_info_disk_fail: OK'
+        
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
