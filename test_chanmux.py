5f860a0084770858f48a98311f39c971f12c500a
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-02-03 18:29:48 +0100

    [DOC] add test descriptions to test_chanmux

--- test_chanmux.py
+++ test_chanmux.py
@@ -14,6 +14,19 @@ timeout = 300
 #@pytest.mark.skip(reason="backend not yet implemented")
 #-------------------------------------------------------------------------------
 def test_chanmux_overflow(boot_with_proxy):
+    """This test will check the correct trigger of the overflow condition of
+    ChanMux.
+
+    The behavior of ChanMux is described at https://wiki.hensoldt-cyber.systems/display/HEN/SEOS+ChanMUX%2C+UART+Proxy+and+Host-Bridge.
+
+    Underlying SEOS Test System behavior:
+
+        two SEOS Tester components will (in parallel) send a command to the
+        Proxy App (via ChanMux) in order to request a shot of an amount of data
+        that will result in an overflow.
+        After that the ChanMux gets probed to check whether the overflow
+        condition happened as expected."""
+
     test_run = boot_with_proxy(test_system)
     f_out = test_run[1]
 
@@ -29,6 +42,17 @@ def test_chanmux_overflow(boot_with_proxy):
 #@pytest.mark.skip(reason="backend not yet implemented")
 #-------------------------------------------------------------------------------
 def test_chanmux_fullduplex(boot_with_proxy):
+    """This test will check the correct operation of ChanMux when streaming in
+    full duplex mode.
+
+    Underlying SEOS Test System behavior:
+
+        two SEOS Tester components will (in parallel) send a command to the
+        Proxy App (via ChanMux) in order to request a stream of a certain
+        amount of data. While the Proxy is streaming its data (with a
+        pre-established pattern) the Testers will check the pattern of the
+        data and echo them 'online'."""
+
     test_run = boot_with_proxy(test_system)
     f_out = test_run[1]
 
