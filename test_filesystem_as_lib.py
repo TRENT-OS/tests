0cbe5dfd8020c295697a4d545bc64399be3cd296
Sebastian Eckl <sebastian.eckl@hensoldt-cyber.com>
2020-02-12 15:48:03 +0100

    [DEV] cleanup & add additional FS tests

--- test_filesystem_as_lib.py
+++ test_filesystem_as_lib.py
@@ -11,16 +11,16 @@ timeout = 15
 # TEST: partition_manager_init()
 #-------------------------------------------------------------------------------
 
-def test_filesystem_init_test(boot_with_proxy):
-    """This test relies on a ChanMuxClient and a ProxyNVM instance to allow the initialization of the NVM storage backend.
+def test_filesystem_hw_init(boot_with_proxy):
+    """This test creates a ChanMuxClient and a ProxyNVM instance to allow the initialization of the NVM storage backend.
+    Both instances provide and represent the underlying HW storage. 
     
     The behavior of the FileSystem is described at https://wiki.hensoldt-cyber.systems/pages/viewpage.action?spaceKey=HEN&title=SEOS+File+System.
 
     Underlying SEOS Test System behavior:
 
         a NVM backend is created at the host system, which can be used as a 
-        disk by the SEOS partition manager. The partition manager uses the existing
-        disk to get the required information."""
+        disk by the SEOS partition manager. """
     proxy_memory_file = "nvm_06"
     if os.path.isfile(proxy_memory_file):
         os.remove(proxy_memory_file)
@@ -38,7 +38,7 @@ def test_filesystem_init_test(boot_with_proxy):
     f_out = test_run[1]
 
     result_list = [
-        'TestFSHWInit_test: OK'
+        'TestFSHWInit: OK'
     ]
     for result in result_list:
         (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
@@ -46,8 +46,36 @@ def test_filesystem_init_test(boot_with_proxy):
 
 #-------------------------------------------------------------------------------
 
+def test_filesystem_partition_manager_init(boot_with_proxy):
+    """ This test initializes the partition manager with the provided storage backend, provided in form of a ProxyNVM instance. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSPartitionManagerInit: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_partition_manager_disk(boot_with_proxy):
+    """ This test creates an abstract disk representation of the physical storage backend. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSPartitionManagerDisk: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
 def test_filesystem_partition_manager_create(boot_with_proxy):
-    """ This test fetches all required partitions from the SEOS system config and initializes them on disk."""
+    """ This test fetches all required partitions from the provided system config and initializes them on the disk."""
     test_run = boot_with_proxy("test_filesystem_as_lib")
     f_out = test_run[1]
 
@@ -61,7 +89,7 @@ def test_filesystem_partition_manager_create(boot_with_proxy):
 #-------------------------------------------------------------------------------
 
 def test_filesystem_partition_manager_open(boot_with_proxy):
-    """ """
+    """ This test opens the created partitions. """
     test_run = boot_with_proxy("test_filesystem_as_lib")
     f_out = test_run[1]
 
@@ -75,7 +103,11 @@ def test_filesystem_partition_manager_open(boot_with_proxy):
 #-------------------------------------------------------------------------------
 
 def test_filesystem_create(boot_with_proxy):
-    """ """
+    """ This test initializes a filesystem instance on each partition available at the system.
+        The following filesystem types are available right now:
+            - FAT (FAT12, FAT16, FAT32)
+            - SPIFFS 
+        Currently, only the FAT filesystem is tested. """
     test_run = boot_with_proxy("test_filesystem_as_lib")
     f_out = test_run[1]
 
@@ -88,13 +120,13 @@ def test_filesystem_create(boot_with_proxy):
 
 #-------------------------------------------------------------------------------
 
-def test_filesystem_mount(boot_with_proxy):
-    """ """
+def test_filesystem_partition_mount(boot_with_proxy):
+    """ This test mounts all created filesystem instances on each of the partitions available. """
     test_run = boot_with_proxy("test_filesystem_as_lib")
     f_out = test_run[1]
 
     result_list = [
-        'TestFSMount: OK'
+        'TestFSPartitionMount: OK'
     ]
     for result in result_list:
         (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
@@ -102,13 +134,97 @@ def test_filesystem_mount(boot_with_proxy):
 
 #-------------------------------------------------------------------------------
 
-def test_filesystem_close(boot_with_proxy):
-    """ """
+def test_filesystem_file_create(boot_with_proxy):
+    """ This test creates different files of different file size and file content on provided partitions. """
     test_run = boot_with_proxy("test_filesystem_as_lib")
     f_out = test_run[1]
 
     result_list = [
-        'TestFSClose: OK'
+        'TestFSFileCreate: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_file_size(boot_with_proxy):
+    """ This test fetches the file size of selected files on provided partitions. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSFileSize: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_file_read(boot_with_proxy):
+    """ This test reads selected files from a provided partition and checks the file content for corruption. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSFileRead: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_file_delete(boot_with_proxy):
+    """ This test deletes selected files from provided partitions. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSFileDelete: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_partition_manager_wipe(boot_with_proxy):
+    """ This test wipes all files from provided partitions. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSPartitionWipe: NOT OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_partition_unmount(boot_with_proxy):
+    """ This test unmounts selected filesystem instances on provided partitions. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSPartitionUnmount: OK'
+    ]
+    for result in result_list:
+        (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
+        assert match == result  
+
+#-------------------------------------------------------------------------------
+
+def test_filesystem_partition_manager_close(boot_with_proxy):
+    """ This test closes provided partitions. """
+    test_run = boot_with_proxy("test_filesystem_as_lib")
+    f_out = test_run[1]
+
+    result_list = [
+        'TestFSPartitionClose: OK'
     ]
     for result in result_list:
         (text,match) = logs.get_match_in_line(f_out,re.compile(result),timeout)
