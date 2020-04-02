e72d57f877b608979a9debf74d2a3877ad64fb00
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-04-02 09:53:36 +0200

    [DEV] SEOS-1065: Removed some testcases from TA script

--- test_keystore.py
+++ test_keystore.py
@@ -20,7 +20,6 @@ def test_key_store_unit_tests_fat(boot_with_proxy):
     """
     Run unit tests (import, read, delete) with KeySrore (using FAT file system)
     Scenario 1: local keystore
-    Scenario 2: remote keystore
     """
 
     test_run = boot_with_proxy(test_system)
@@ -30,7 +29,6 @@ def test_key_store_unit_tests_fat(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreFAT_scenario_1 succeeded",
-            "TestKeyStoreFAT_scenario_2 succeeded"
         ],
         timeout)
 
@@ -43,7 +41,6 @@ def test_key_store_integration_tests_AES_fat(boot_with_proxy):
     """
     import AES-256 key to KeyStore using (using FAT file system)
     Scenario 3: local keystore
-    Scenario 4: remote keystore
     """
 
     test_run = boot_with_proxy(test_system)
@@ -53,7 +50,6 @@ def test_key_store_integration_tests_AES_fat(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreFAT_scenario_3 succeeded",
-            "TestKeyStoreFAT_scenario_4 succeeded"
         ],
         timeout)
 
@@ -66,7 +62,6 @@ def test_key_store_integration_tests_keyPair_fat(boot_with_proxy):
     """
     import RSA-128 key pair to KeyStore using FAT file system
     Scenario 5: local keystore
-    Scenario 6: remote keystore
     """
 
     test_run = boot_with_proxy(test_system)
@@ -76,7 +71,6 @@ def test_key_store_integration_tests_keyPair_fat(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreFAT_scenario_5 succeeded",
-            "TestKeyStoreFAT_scenario_6 succeeded"
         ],
         timeout)
 
@@ -89,7 +83,6 @@ def test_key_store_multi_instance_tests_copy_fat(boot_with_proxy):
     """
     Copy key from one keystore to another (using FAT file system)
     Scenario 7: local keystore
-    Scenario 8: remote keystore
     """
 
     test_run = boot_with_proxy(test_system)
@@ -99,7 +92,6 @@ def test_key_store_multi_instance_tests_copy_fat(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreFAT_scenario_7 succeeded",
-            "TestKeyStoreFAT_scenario_8 succeeded"
         ],
         timeout)
 
@@ -112,7 +104,6 @@ def test_key_store_multi_instance_tests_move_fat(boot_with_proxy):
     """
     Move key from one keystore to another (using FAT file system)
     Scenario 9: local keystore
-    Scenario 10: remote keystore
     """
 
     test_run = boot_with_proxy(test_system)
@@ -122,7 +113,6 @@ def test_key_store_multi_instance_tests_move_fat(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreFAT_scenario_9 succeeded",
-            "TestKeyStoreFAT_scenario_10 succeeded"
         ],
         timeout
         )
@@ -137,10 +127,6 @@ def test_key_store_unit_tests_spiffs(boot_with_proxy):
     """
     Run unit tests (import, read, delete) with KeySrore (using SPIFFS file system)
     Scenario 1: local keystore
-    Scenario 2: remote keystore
-
-        - STATUS: NOT WORKING
-        - PROBLEM: SPIFFS integration has to be reworked first
     """
 
     test_run = boot_with_proxy(test_system)
@@ -150,7 +136,6 @@ def test_key_store_unit_tests_spiffs(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreSPIFFS_scenario_1 succeeded",
-            "TestKeyStoreSPIFFS_scenario_2 succeeded"
         ],
         timeout)
 
@@ -163,10 +148,6 @@ def test_key_store_integration_tests_AES_spiffs(boot_with_proxy):
     """
     import AES-256 key to KeyStore using (using SPIFFS file system)
     Scenario 3: local keystore
-    Scenario 4: remote keystore
-
-        - STATUS: NOT WORKING
-        - PROBLEM: SPIFFS integration has to be reworked first
     """
 
     test_run = boot_with_proxy(test_system)
@@ -176,7 +157,6 @@ def test_key_store_integration_tests_AES_spiffs(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreSPIFFS_scenario_3 succeeded",
-            "TestKeyStoreSPIFFS_scenario_4 succeeded"
         ],
         timeout)
 
@@ -189,10 +169,6 @@ def test_key_store_integration_tests_keyPair_spiffs(boot_with_proxy):
     """
     import RSA-128 key pair to KeyStore (using SPIFFS file system)
     Scenario 5: local keystore
-    Scenario 6: remote keystore
-
-        - STATUS: NOT WORKING
-        - PROBLEM: SPIFFS integration has to be reworked first
     """
 
     test_run = boot_with_proxy(test_system)
@@ -202,7 +178,6 @@ def test_key_store_integration_tests_keyPair_spiffs(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreSPIFFS_scenario_5 succeeded",
-            "TestKeyStoreSPIFFS_scenario_6 succeeded"
         ],
         timeout)
 
@@ -215,10 +190,6 @@ def test_key_store_multi_instance_tests_copy_spiffs(boot_with_proxy):
     """
     Copy key from one keystore to another (using SPIFFS file system)
     Scenario 7: local keystore
-    Scenario 8: remote keystore
-
-        - STATUS: NOT WORKING
-        - PROBLEM: SPIFFS integration has to be reworked first
     """
 
     test_run = boot_with_proxy(test_system)
@@ -228,7 +199,6 @@ def test_key_store_multi_instance_tests_copy_spiffs(boot_with_proxy):
         f_out,
         [
             "TestKeyStoreSPIFFS_scenario_7 succeeded",
-            "TestKeyStoreSPIFFS_scenario_8 succeeded"
         ],
         timeout)
 
@@ -241,10 +211,6 @@ def test_key_store_multi_instance_tests_move_spiffs(boot_with_proxy):
     """
     Move key from one keystore to another (using SPIFFS file system)
     Scenario 9: local keystore
-    Scenario 10: remote keystore
-
-        - STATUS: NOT WORKING
-        - PROBLEM: SPIFFS integration has to be reworked first
     """
 
     test_run = boot_with_proxy(test_system)
@@ -254,7 +220,6 @@ def test_key_store_multi_instance_tests_move_spiffs(boot_with_proxy):
     f_out,
     [
         "TestKeyStoreSPIFFS_scenario_9 succeeded",
-        "TestKeyStoreSPIFFS_scenario_10 succeeded"
     ],
     timeout)
 
