b5a826a68c10558e85458e61edb133b30616409d
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-07-13 11:44:03 +0200

    [TA] Add storage underflow address test

--- test_proxy_nvm.py
+++ test_proxy_nvm.py
@@ -139,6 +139,39 @@ def test_proxy_nvm_storage_overflow(boot_with_proxy):
 
         assert match == success_msg
 
+def test_proxy_nvm_storage_underflow(boot_with_proxy):
+    """This test will check that writing outside of the bounds (when starting
+    underflowed address) results in an error condition.
+
+    Underlying SEOS Test System behavior:
+
+        Two SEOS Tester components will (in parallel) write a data via the NVM
+        abstraction exposed by the Proxy app with a known pattern and then read
+        it and verify it.
+
+    Test is currently limited to the disk size preconfigured by the proxy
+    application:
+        - 36  MB for 1st NVM channel
+        - 128 KB for 2nd NVM channel
+    """
+
+    test_run = boot_with_proxy(test_system)
+    f_out = test_run[1]
+
+    expected_output_array = [
+        #every test case output needs to happen twice (for both channels)
+        "TEST STORAGE UNDERFLOW: Write failed!",
+        "TEST STORAGE UNDERFLOW: Write failed!"
+    ]
+
+    for success_msg in expected_output_array:
+        (text, match) = logs.get_match_in_line(
+                                f_out,
+                                re.compile(success_msg),
+                                timeout)
+
+        assert match == success_msg
+
 def test_proxy_nvm_far_out_of_bounds_address(boot_with_proxy):
     """This test will check that writing outside of the bounds (invalid start
     addresses) results in an error condition.
