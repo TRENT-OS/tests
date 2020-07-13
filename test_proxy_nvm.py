4b44743518b0ff32fe5473becd360c48922c216d
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-07-13 11:31:44 +0200

    [TA] Add storage mid address test

--- test_proxy_nvm.py
+++ test_proxy_nvm.py
@@ -42,6 +42,38 @@ def test_proxy_nvm_storage_start_address(boot_with_proxy):
 
         assert match == success_msg
 
+def test_proxy_nvm_storage_mid_address(boot_with_proxy):
+    """This test will check writing and reading in the middle of the storage.
+
+    Underlying SEOS Test System behavior:
+
+        Two SEOS Tester components will (in parallel) write a data via the NVM
+        abstraction exposed by the Proxy app with a known pattern and then read
+        it and verify it.
+
+    Test is currently limited to the disk size preconfigured by the proxy
+    application:
+        - 36  MB for 1st NVM channel
+        - 128 KB for 2nd NVM channel
+    """
+
+    test_run = boot_with_proxy(test_system)
+    f_out = test_run[1]
+
+    expected_output_array = [
+        #every test case output needs to happen twice (for both channels)
+        "TEST STORAGE MID ADDRESS: Read values match the write values!",
+        "TEST STORAGE MID ADDRESS: Read values match the write values!"
+    ]
+
+    for success_msg in expected_output_array:
+        (text, match) = logs.get_match_in_line(
+                                f_out,
+                                re.compile(success_msg),
+                                timeout)
+
+        assert match == success_msg
+
 def test_proxy_nvm_storage_end_address(boot_with_proxy):
     """This test will check writing and reading at the end of the storage.
 
