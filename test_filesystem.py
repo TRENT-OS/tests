bcb40ed77a4ba155d218aa4e7c23bb280bf10b0e
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-07-17 17:45:56 +0200

    [DEV] Add test checks for spiffs

--- test_filesystem.py
+++ test_filesystem.py
@@ -114,3 +114,57 @@ def test_OS_FileSystem_unmount_1(boot):
     <TODO: Describe here what the test does>
     """
     parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystem_unmount')
+
+def test_OS_FileSystem_format_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystem_format')
+
+def test_OS_FileSystem_mount_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystem_mount')
+
+def test_OS_FileSystemFile_open_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_open')
+
+def test_OS_FileSystemFile_write_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_write')
+
+def test_OS_FileSystemFile_read_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_read')
+
+def test_OS_FileSystemFile_close_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_close')
+
+def test_OS_FileSystemFile_getSize_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_getSize')
+
+def test_OS_FileSystemFile_delete_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystemFile_delete')
+
+def test_OS_FileSystem_unmount_2(boot):
+    """
+    <TODO: Describe here what the test does>
+    """
+    parser.check_test(boot(TEST_NAME), 3, 'test_OS_FileSystem_unmount')
