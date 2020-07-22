d249fa3fef1f03c43ffc7d28455765c5e47a788e
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-07-22 20:22:56 +0200

    [REF] Switch the order of storage test
    Test runner first executes RAM Disk tests (of index 0), so the banners were
    misleading and test names were misleading.

--- test_storage_interface.py
+++ test_storage_interface.py
@@ -9,93 +9,6 @@ TEST_NAME = 'test_storage_interface'
 TEST_TIMEOUT = 10
 
 """
-             #####                          #     #
-            #     # #    #   ##   #    #    ##   ## #    # #    #
-            #       #    #  #  #  ##   #    # # # # #    #  #  #
-            #       ###### #    # # #  #    #  #  # #    #   ##
-            #       #    # ###### #  # #    #     # #    #   ##
-            #     # #    # #    # #   ##    #     # #    #  #  #
-             #####  #    # #    # #    #    #     #  ####  #    #
-"""
-
-def test_storage_size_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Checks if the storage is big enough i.e. three times greater than the test
-    string, so that the test string can be written at the beginning, in the
-    center and at the end of it.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_size_pos', 'idx=0')
-
-def test_storage_writeReadEraseBegin_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases the test string at the beginning of the storage.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseBegin_pos', 'idx=0')
-
-def test_storage_writeReadEraseMid_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases the test string in the center of the storage.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseMid_pos', 'idx=0')
-
-def test_storage_writeReadEraseEnd_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases the test string at the end of the storage.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseEnd_pos', 'idx=0')
-
-def test_storage_writeReadEraseZeroBytes_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases zero bytes in the storage.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseZeroBytes_pos', 'idx=0')
-
-def test_storage_writeReadEraseOutside_neg_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases outside of the storage area expecting the storage
-    to report an error in that case.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseOutside_neg', 'idx=0')
-
-def test_storage_writeReadEraseTooLarge_neg_chanmuxStorage(boot_with_proxy):
-    """
-    ChanMuxStorage driver shall validate input paramaters and do not allow
-    write, reads, and erases that are too large.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_writeReadEraseTooLarge_neg', 'idx=0')
-
-def test_storage_neighborRegionsUntouched_pos_chanmuxStorage(boot_with_proxy):
-    """
-    Writes, reads and erases the test string, and verifies that the data in
-    front of it and at the back were not corrupted.
-    """
-    parser.check_test(
-        boot_with_proxy(TEST_NAME),
-        TEST_TIMEOUT,
-        'test_storage_neighborRegionsUntouched_pos', 'idx=0')
-
-"""
         ########.....###....##.....##       ########..####..######..##....##
         ##.....##...##.##...###...###       ##.....##..##..##....##.##...##.
         ##.....##..##...##..####.####       ##.....##..##..##.......##..##..
@@ -114,7 +27,7 @@ def test_storage_size_pos_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_size_pos', 'idx=1')
+        'test_storage_size_pos', 'idx=0')
 
 def test_storage_writeReadEraseBegin_pos_ramDisk(boot_with_proxy):
     """
@@ -123,7 +36,7 @@ def test_storage_writeReadEraseBegin_pos_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseBegin_pos', 'idx=1')
+        'test_storage_writeReadEraseBegin_pos', 'idx=0')
 
 def test_storage_writeReadEraseMid_pos_ramDisk(boot_with_proxy):
     """
@@ -132,7 +45,7 @@ def test_storage_writeReadEraseMid_pos_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseMid_pos', 'idx=1')
+        'test_storage_writeReadEraseMid_pos', 'idx=0')
 
 def test_storage_writeReadEraseEnd_pos_ramDisk(boot_with_proxy):
     """
@@ -141,7 +54,7 @@ def test_storage_writeReadEraseEnd_pos_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseEnd_pos', 'idx=1')
+        'test_storage_writeReadEraseEnd_pos', 'idx=0')
 
 def test_storage_writeReadEraseZeroBytes_pos_ramDisk(boot_with_proxy):
     """
@@ -150,7 +63,7 @@ def test_storage_writeReadEraseZeroBytes_pos_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseZeroBytes_pos', 'idx=1')
+        'test_storage_writeReadEraseZeroBytes_pos', 'idx=0')
 
 def test_storage_writeReadEraseOutside_neg_ramDisk(boot_with_proxy):
     """
@@ -160,7 +73,7 @@ def test_storage_writeReadEraseOutside_neg_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseOutside_neg', 'idx=1')
+        'test_storage_writeReadEraseOutside_neg', 'idx=0')
 
 def test_storage_writeReadEraseTooLarge_neg_ramDisk(boot_with_proxy):
     """
@@ -170,9 +83,96 @@ def test_storage_writeReadEraseTooLarge_neg_ramDisk(boot_with_proxy):
     parser.check_test(
         boot_with_proxy(TEST_NAME),
         TEST_TIMEOUT,
-        'test_storage_writeReadEraseTooLarge_neg', 'idx=1')
+        'test_storage_writeReadEraseTooLarge_neg', 'idx=0')
 
 def test_storage_neighborRegionsUntouched_pos_ramDisk(boot_with_proxy):
+    """
+    Writes, reads and erases the test string, and verifies that the data in
+    front of it and at the back were not corrupted.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_neighborRegionsUntouched_pos', 'idx=0')
+
+"""
+             #####                          #     #
+            #     # #    #   ##   #    #    ##   ## #    # #    #
+            #       #    #  #  #  ##   #    # # # # #    #  #  #
+            #       ###### #    # # #  #    #  #  # #    #   ##
+            #       #    # ###### #  # #    #     # #    #   ##
+            #     # #    # #    # #   ##    #     # #    #  #  #
+             #####  #    # #    # #    #    #     #  ####  #    #
+"""
+
+def test_storage_size_pos_chanmuxStorage(boot_with_proxy):
+    """
+    Checks if the storage is big enough i.e. three times greater than the test
+    string, so that the test string can be written at the beginning, in the
+    center and at the end of it.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_size_pos', 'idx=1')
+
+def test_storage_writeReadEraseBegin_pos_chanmuxStorage(boot_with_proxy):
+    """
+    Writes, reads and erases the test string at the beginning of the storage.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseBegin_pos', 'idx=1')
+
+def test_storage_writeReadEraseMid_pos_chanmuxStorage(boot_with_proxy):
+    """
+    Writes, reads and erases the test string in the center of the storage.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseMid_pos', 'idx=1')
+
+def test_storage_writeReadEraseEnd_pos_chanmuxStorage(boot_with_proxy):
+    """
+    Writes, reads and erases the test string at the end of the storage.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseEnd_pos', 'idx=1')
+
+def test_storage_writeReadEraseZeroBytes_pos_chanmuxStorage(boot_with_proxy):
+    """
+    Writes, reads and erases zero bytes in the storage.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseZeroBytes_pos', 'idx=1')
+
+def test_storage_writeReadEraseOutside_neg_chanmuxStorage(boot_with_proxy):
+    """
+    Writes, reads and erases outside of the storage area expecting the storage
+    to report an error in that case.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseOutside_neg', 'idx=1')
+
+def test_storage_writeReadEraseTooLarge_neg_chanmuxStorage(boot_with_proxy):
+    """
+    ChanMuxStorage driver shall validate input paramaters and do not allow
+    write, reads, and erases that are too large.
+    """
+    parser.check_test(
+        boot_with_proxy(TEST_NAME),
+        TEST_TIMEOUT,
+        'test_storage_writeReadEraseTooLarge_neg', 'idx=1')
+
+def test_storage_neighborRegionsUntouched_pos_chanmuxStorage(boot_with_proxy):
     """
     Writes, reads and erases the test string, and verifies that the data in
     front of it and at the back were not corrupted.
