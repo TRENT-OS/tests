TRENT-OS/tests

commit a0b401481c426f2b3beff601dd01ef809e8ac25c

[DEV] SEOS-3195 Implement retry for boot check with exponential backoff

conftest.py
@@ -73,7 +73,21 @@ def start_or_attach_to_test_runner(
 
         test_runner.start()
 
-        test_runner.check_start_success(boot_mode)
+        retries = 4
+
+        sleep_time = 1
+
+        for x in range(1, retries + 1):
+            try:
+                test_runner.check_start_success(boot_mode)
+            except Exception as e:
+                if (x >= retries):
+                    raise e
+                print('Start not detected. Retrying after {} seconds'.format(sleep_time))
+                time.sleep(sleep_time)
+                sleep_time *= 2
+                continue
+            break
 
         # pytest will receive the tupel from this "callback" for each test
         # case. The "system" parameter that the test passes in the call is

commit 28976dec87e96a35fcbb37fda59b8583c9268fcc

[DEV] SEOS-3298: Expand the retry block to include errors in the test_runner initialization
Expand the retry block introduced in the commit a0b401481c426f2b3beff601dd01ef809e8ac25c to include test_runner initialization using the get_test_runner() function and unify exception handling. This solves the issue of some interfaces temporarily not being ready at the time of the first initialization (e.g. tap2 for sabre when running multiple test scripts in succession).

conftest.py
@@ -59,51 +59,47 @@ def start_or_attach_to_test_runner(
     is_error = False
     test_runner = None
 
-    try:
-        test_runner = board_automation.system_selector.get_test_runner(
-                        log_dir,
-                        resource_dir,
-                        platform,
-                        system_image,
-                        proxy_config,
-                        sd_card_size,
-                        additional_params,
-                        print_logs
-                    )
+    retries = 4
+    sleep_time = 1
 
-        test_runner.start()
+    for x in range(1, retries + 1):
+        try:
+            test_runner = board_automation.system_selector.get_test_runner(
+                            log_dir,
+                            resource_dir,
+                            platform,
+                            system_image,
+                            proxy_config,
+                            sd_card_size,
+                            additional_params,
+                            print_logs
+                        )
 
-        retries = 4
+            test_runner.start()
+            test_runner.check_start_success(boot_mode)
 
-        sleep_time = 1
-
-        for x in range(1, retries + 1):
-            try:
-                test_runner.check_start_success(boot_mode)
-            except Exception as e:
-                if (x >= retries):
-                    raise e
-                print('Start not detected. Retrying after {} seconds'.format(sleep_time))
+            # pytest will receive the tupel from this "callback" for each test
+            # case. The "system" parameter that the test passes in the call is
+            # no longer used, since the system image is passed as parameter
+            # when the whole test framework is started.
+            yield (lambda system = None: (
+                                test_runner,
+                                test_runner.get_system_log() # kept for legacy compatibility
+                            ) )
+        except:
+            if (x >= retries):
+                # catch really *all* exceptions
+                exc_info = sys.exc_info()
+                msg = exc_info[1]
+                print('Test_runner exception: {}'.format(msg))
+                traceback.print_exception(*exc_info)
+                is_error = True
+            else:
+                print('Succesful start not detected. Retrying after {} seconds'.format(sleep_time))
                 time.sleep(sleep_time)
                 sleep_time *= 2
                 continue
-            break
-
-        # pytest will receive the tupel from this "callback" for each test
-        # case. The "system" parameter that the test passes in the call is
-        # no longer used, since the system image is passed as parameter
-        # when the whole test framework is started.
-        yield (lambda system = None: (
-                            test_runner,
-                            test_runner.get_system_log() # kept for legacy compatibility
-                        ) )
-
-    except: # catch really *all* exceptions
-        exc_info = sys.exc_info()
-        msg = exc_info[1]
-        print('test_runner exception: {}'.format(msg))
-        traceback.print_exception(*exc_info)
-        is_error = True
+        break
 
     if test_runner:
         try: