1716312db300b46f87624cf12622844d598d293e
Axel Heider <axel.heider@hensoldt.net>
2022-08-08 19:39:06 +0200

    [DEV] fix expected order, since the driver has a higher priority

--- test_uart.py
+++ test_uart.py
@@ -38,8 +38,8 @@ def test_uart(boot):
     # evidence. System load can likely impact this timing.
     (ret, idx) = test_runner.system_log_match_sequence(
         [
-            'UART tester loop running',
             'initialize UART ok',
+            'UART tester loop running',
         ],
         10)
 
