b473aa815400c5b852a963bd18e39f6896b83eb3
Axel Heider <axel.heider@hensoldt.net>
2023-04-21 19:55:17 +0200

    WIP test for VM demos

---
+++ test_demo_vm_drone_sim.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2023, HENSOLDT Cyber GmbH
+#
+
+import pytest
+
+
+#-------------------------------------------------------------------------------
+def test_demo_vm_drone_sim(boot):
+    """
+    Test a VirtIO VM system
+    """
+
+    test_runner = boot()
+    ret = test_runner.system_log_match(
+            ( [
+                 'Ping test was successful',
+                 'Welcome to Buildroot'
+              ], 60 )
+          )
+    if not ret.ok:
+        pytest.fail(f'missing string: {ret.get_missing()}')
